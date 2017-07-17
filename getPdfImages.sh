2d02379172b605f502dcb4f164b3e9f8ae631137
brianjester <brianjester@protonmail.com>
2017-07-16 19:36:01 -0700

    Update getPdfImages.sh

--- getPdfImages.sh
+++ getPdfImages.sh
@@ -2,15 +2,17 @@
 # getImages.sh - script to get images from a PDF
 PDF_URL=https://www.e-churchbulletins.com/bulletins/
 PDF_NAME=977090
+MY_HOME=/Users/bjester.ctr/brianjester
 IMAGE_DIR=images
 FB_POST=http://brianjester.github.io/index.html
 #DATE=`date +%Y-%m-%d`
 #mkdir ${DATE}
-curl ${PDF_URL}${PDF_NAME}.pdf > ${PDF_NAME}.pdf
-rm -rf ${IMAGE_DIR}/*
-pdfimages -f 1 -l 2 -all ${PDF_NAME}.pdf ${IMAGE_DIR}/${PDF_NAME}
-rm *.pdf
-cd ${IMAGE_DIR}
+cd ${MY_HOME}
+curl ${PDF_URL}${PDF_NAME}.pdf > ${MY_HOME}/${PDF_NAME}.pdf
+rm -rf ${IMAGE_DIR}/*.jpg
+pdfimages -f 1 -l 2 -all ${MY_HOME}/${PDF_NAME}.pdf ${MY_HOME}/${IMAGE_DIR}/${PDF_NAME}
+rm ${MY_HOME}/${PDF_NAME}.pdf
+cd ${MY_HOME}/${IMAGE_DIR}
 rm *.params *.ccitt
 for file in `find . -size -100k | grep -v "^\.$"`
 do
@@ -24,7 +26,7 @@ do
   let INDEX+=1
 done
 ls
-cd ..
+cd ${MY_HOME}
 git add -A
 git commit -m "Updated images"
 git push
