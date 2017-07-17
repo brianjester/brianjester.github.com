5232cc431fa811b512fd186cef78bef9d7a8fbac
brianjester <brianjester@protonmail.com>
2017-07-16 20:07:33 -0700

    Update getPdfImages.sh

--- getPdfImages.sh
+++ getPdfImages.sh
@@ -7,27 +7,27 @@ IMAGE_DIR=images
 FB_POST=http://brianjester.github.io/index.html
 #DATE=`date +%Y-%m-%d`
 #mkdir ${DATE}
-cd ${MY_HOME}
 curl ${PDF_URL}${PDF_NAME}.pdf > ${MY_HOME}/${PDF_NAME}.pdf
-rm -rf ${IMAGE_DIR}/*.jpg
+rm -rf ${MY_HOME}/${IMAGE_DIR}
+mkdir ${MY_HOME}/${IMAGE_DIR}
 pdfimages -f 1 -l 2 -all ${MY_HOME}/${PDF_NAME}.pdf ${MY_HOME}/${IMAGE_DIR}/${PDF_NAME}
 rm ${MY_HOME}/${PDF_NAME}.pdf
-cd ${MY_HOME}/${IMAGE_DIR}
-rm *.params *.ccitt
-for file in `find . -size -100k | grep -v "^\.$"`
+rm ${MY_HOME}/${IMAGE_DIR}/*.params 
+rm ${MY_HOME}/${IMAGE_DIR}/*.ccitt
+for file in `find ${MY_HOME}/${IMAGE_DIR} -size -100k | grep -v "^\.$"`
 do
   rm $file
 done
 ls
 INDEX=1
-for file in `find . | grep -v "^\.$"`
+for file in `find ${MY_HOME}/${IMAGE_DIR} | grep -v "^\.$"`
 do
-  mv $file image${INDEX}.jpg
+  mv $file ${MY_HOME}/${IMAGE_DIR}/image${INDEX}.jpg
   let INDEX+=1
 done
 ls
 cd ${MY_HOME}
-git add -A
-git commit -m "Updated images"
+git add ${MY_HOME}
+git commit -m "Updated images" -- ${MY_HOME}
 git push
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-gpu http://brianjester.github.io/index.html
