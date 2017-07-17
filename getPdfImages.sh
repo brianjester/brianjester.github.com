bf654018dfa8dce09eae874c0f1f96c7b374d1eb
Brian Jester <bjester.ctr@palantir.com>
2017-07-16 17:58:56 -0700

    adding getPdfImages code

---
+++ getPdfImages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# getImages.sh - script to get images from a PDF
+PDF_URL=https://www.e-churchbulletins.com/bulletins/
+PDF_NAME=977090
+IMAGE_DIR=images
+#DATE=`date +%Y-%m-%d`
+#mkdir ${DATE}
+curl ${PDF_URL}${PDF_NAME}.pdf > ${PDF_NAME}.pdf
+rm -rf ${IMAGE_DIR}/*
+pdfimages -f 1 -l 2 -all ${PDF_NAME}.pdf ${IMAGE_DIR}/${PDF_NAME}
+rm *.pdf
+cd ${IMAGE_DIR}
+rm *.params *.ccitt
+for file in `find . -size -100k | grep -v "^\.$"`
+do
+  rm $file
+done
+ls
+INDEX=1
+for file in `find . | grep -v "^\.$"`
+do
+  mv $file image${INDEX}.jpg
+  let INDEX+=1
+done
+ls
