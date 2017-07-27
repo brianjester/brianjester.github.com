f840862ab9c1e331e51e25777320ded0080bae03
Brian Jester <brian.jester@pvmit.com>
2017-07-26 20:21:26 -0700

    Updated 52 week lows

--- 52weekLow.sh
+++ 52weekLow.sh
@@ -22,6 +22,14 @@ HEADER="<HTML>\
 <script>\
      (adsbygoogle = window.adsbygoogle || []).push({});\
 </script>\
+<script>\
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');\
+  ga('create', 'UA-25680902-2', 'auto');\
+  ga('send', 'pageview');\
+</script>\
 <H1>52 Week Lows: NASDAQ, NYSE, AMEX</H1><UL>"
 
 FOOTER="<script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>\
