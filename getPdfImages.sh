8b5277483744ee3adef0fab04db5887e1081b538
Brian Jester <brianjester@protonmail.com>
2018-07-29 22:45:04 -0700

    Updated for RasPi

--- getPdfImages.sh
+++ getPdfImages.sh
@@ -2,21 +2,23 @@
 # getImages.sh - script to get images from a PDF
 PDF_URL=https://www.e-churchbulletins.com/bulletins/
 PDF_NAME=977090
-MY_HOME=/Users/brian/brianjester.github.com
+MY_HOME=/home/pi/brianjester.github.com
 IMAGE_DIR=images
 FB_POST=https://brianjester.github.io/index.html
+POPPLER=/usr/bin/pdfimages
 #DATE=`date +%Y-%m-%d`
 #mkdir ${DATE}
-rm ${MY_HOME}/${IMAGE_DIR}/*.jpg
+#rm ${MY_HOME}/${IMAGE_DIR}/*.jpg
 curl ${PDF_URL}${PDF_NAME}.pdf > ${MY_HOME}/${PDF_NAME}.pdf
-if [ ! -f /usr/local/bin/pdfimages ]; then
+if [ ! -f $POPPLER ]; then
   echo "No poppler package (pdfimages) found."
   exit 1
 fi
-/usr/local/bin/pdfimages  -f 1 -l 1 -all ${MY_HOME}/${PDF_NAME}.pdf ${MY_HOME}/${IMAGE_DIR}/${PDF_NAME}
+$POPPLER  -f 1 -l 1 -all ${MY_HOME}/${PDF_NAME}.pdf ${MY_HOME}/${IMAGE_DIR}/${PDF_NAME}
 rm ${MY_HOME}/${PDF_NAME}.pdf
 rm ${MY_HOME}/${IMAGE_DIR}/*.params 
 rm ${MY_HOME}/${IMAGE_DIR}/*.ccitt
+ls ${MY_HOME}/${IMAGE_DIR}
 for file in `find ${MY_HOME}/${IMAGE_DIR} -size -100k | grep -v "^${MY_HOME}/${IMAGE_DIR}$"`
 do
   rm $file
@@ -33,4 +35,5 @@ cd ${MY_HOME}
 git add ${MY_HOME}
 git commit -m "Updated images" -- ${MY_HOME}
 git push
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-gpu https://brianjester.github.io/index.html?p=REDACTED
+#/usr/lib/chromium-browser/chromium-browser --disable-gpu https://brianjester.github.io/index.html?p=Abcd_1234
+/usr/lib/chromium-browser/chromium-browser https://brianjester.github.io/index.html?p=__REDACTED__
